e40f0151f4065690863d0712c80e36d3b7efd7ab
Nihilx <nihilx@tutanota.com>
2025-07-16 00:11:56 +0200

    initial commit

---
+++ _next/static/chunks/app/layout-829e46c424061d64.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{4817:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,9461,23)),Promise.resolve().then(l.t.bind(l,9324,23))},9324:()=>{},9461:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_2270a5"}}},e=>{e.O(0,[20,441,964,358],()=>e(e.s=4817)),_N_E=e.O()}]);
